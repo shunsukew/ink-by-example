8e8310baa0620cdb0228ebf6aaf1929db3e984ac
shunsukew <shunsukesan.tech@gmail.com>
2022-04-18 17:59:48 +0900

    multisig wallet

--- multi_sig_wallet/lib.rs
+++ multi_sig_wallet/lib.rs
@@ -4,71 +4,230 @@ use ink_lang as ink;
 
 #[ink::contract]
 mod multi_sig_wallet {
+    use ink_prelude::{
+        vec::Vec,
+    };
 
-    /// Defines the storage of your contract.
-    /// Add new fields to the below struct in order
-    /// to add new static storage fields to your contract.
+    use ink_storage::{
+        traits::{
+            PackedLayout,
+            SpreadAllocate,
+            SpreadLayout,
+        },
+        Mapping,
+    };
+
+    #[derive(Debug, PartialEq, Eq, scale::Encode, scale::Decode)]
+    #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
+    pub enum Error {
+        InvalidTransactionId,
+        InvalidOwner,
+        TransactionAlreadyConfirmed,
+        TransactionNotConfirmed,
+        TransactionAlreadyExecuted,
+        InsufficientConfirmations,
+        TransactionFailed,
+    }
+
+    pub type Result<T> = core::result::Result<T, Error>;
+
+    #[ink(event)]
+    pub struct SubmitTransaction {
+        owner: AccountId,
+        transaction_id: TransactionId,
+        to: AccountId,
+        value: Balance,
+    }
+
+    #[ink(event)]
+    pub struct ConfirmTransaction {
+        owner: AccountId,
+        transaction_id: u32,
+    }
+
+    #[ink(event)]
+    pub struct RevokeConfirmation {
+        owner: AccountId,
+        transaction_id: u32,
+    }
+
+    type TransactionId = u32;
+
+    #[derive(scale::Encode, scale::Decode, SpreadLayout, PackedLayout)]
+    #[cfg_attr(
+        feature = "std",
+        derive(
+            Debug,
+            PartialEq,
+            Eq,
+            scale_info::TypeInfo,
+            ink_storage::traits::StorageLayout
+        )
+    )]
+    pub struct Transaction {
+        to: AccountId,
+        value: Balance,
+        executed: bool,
+        num_confirmations: u32,
+    }
+
     #[ink(storage)]
+    #[derive(SpreadAllocate)]
     pub struct MultiSigWallet {
-        /// Stores a single `bool` value on the storage.
-        value: bool,
+        threshold: u32,
+        owners: Mapping<AccountId, ()>,
+        transactions: Mapping<TransactionId, Transaction>,
+        confirmations: Mapping<(TransactionId, AccountId), ()>,
+        next_transaction_id: TransactionId,
     }
 
     impl MultiSigWallet {
-        /// Constructor that initializes the `bool` value to the given `init_value`.
         #[ink(constructor)]
-        pub fn new(init_value: bool) -> Self {
-            Self { value: init_value }
+        pub fn new(mut owners: Vec<AccountId>, threshold: u32) -> Self {
+            ink_lang::utils::initialize_contract(|contract: &mut Self| {
+                owners.dedup();
+                assert!(0 < threshold && threshold <= owners.len() as u32);
+
+                for owner in &owners {
+                    contract.owners.insert(owner, &());
+                }
+
+                contract.threshold = threshold;
+                contract.transactions = Default::default();
+                contract.confirmations = Default::default();
+                contract.next_transaction_id = 0;
+            })
         }
 
-        /// Constructor that initializes the `bool` value to `false`.
-        ///
-        /// Constructors can delegate to other constructors.
-        #[ink(constructor)]
-        pub fn default() -> Self {
-            Self::new(Default::default())
+        #[ink(message, payable)]
+        pub fn receive(&mut self) {
+            ink_env::debug_println!(
+                "received payment: {}",
+                self.env().transferred_value()
+            );
         }
 
-        /// A message that can be called on instantiated contracts.
-        /// This one flips the value of the stored `bool` from `true`
-        /// to `false` and vice versa.
         #[ink(message)]
-        pub fn flip(&mut self) {
-            self.value = !self.value;
+        pub fn submit_transaction(&mut self, to: AccountId, value: Balance) -> Result<()> {
+            let caller = Self::env().caller();
+            self.ensure_owner(caller)?;
+
+            let transaction_id = self.next_transaction_id;
+            self.next_transaction_id += 1;
+            self.transactions.insert(transaction_id, &Transaction {
+                to: to,
+                value: value,
+                executed: false,
+                num_confirmations: 0,
+            });
+
+            Self::env().emit_event(SubmitTransaction {
+                owner: caller,
+                transaction_id: transaction_id,
+                to: to,
+                value: value,
+            });
+
+            Ok(())
         }
 
-        /// Simply returns the current value of our `bool`.
         #[ink(message)]
-        pub fn get(&self) -> bool {
-            self.value
+        pub fn confirm_transaction(&mut self, transaction_id: TransactionId) -> Result<()> {
+            let caller = Self::env().caller();
+            self.ensure_owner(caller)?;
+
+            let confirmation_key = (transaction_id, caller);
+            if self.confirmations.get(confirmation_key).is_some() {
+                return Err(Error::TransactionAlreadyConfirmed)
+            };
+
+            self.confirmations.insert(confirmation_key, &());
+
+            let mut transaction = self.transactions.get(transaction_id)
+                .ok_or(Error::InvalidTransactionId)?;
+        
+            if transaction.executed {
+                return Err(Error::TransactionAlreadyExecuted)
+            }
+
+            transaction.num_confirmations += 1;
+            self.transactions.insert(transaction_id, &transaction);
+
+
+            Self::env().emit_event(ConfirmTransaction {
+                owner: caller,
+                transaction_id: transaction_id,
+            });
+
+            Ok(())
         }
-    }
 
-    /// Unit tests in Rust are normally defined within such a `#[cfg(test)]`
-    /// module and test functions are marked with a `#[test]` attribute.
-    /// The below code is technically just normal Rust code.
-    #[cfg(test)]
-    mod tests {
-        /// Imports all the definitions from the outer scope so we can use them here.
-        use super::*;
+        #[ink(message)]
+        pub fn revoke_cofirmation(&mut self, transaction_id: TransactionId) -> Result<()> {
+            let caller = Self::env().caller();
+            self.ensure_owner(caller)?;
 
-        /// Imports `ink_lang` so we can use `#[ink::test]`.
-        use ink_lang as ink;
+            let confirmation_key = (transaction_id, caller);
+            if self.confirmations.get(confirmation_key).is_none() {
+                return Err(Error::TransactionNotConfirmed)
+            };
 
-        /// We test if the default constructor does its job.
-        #[ink::test]
-        fn default_works() {
-            let multi_sig_wallet = MultiSigWallet::default();
-            assert_eq!(multi_sig_wallet.get(), false);
+            self.confirmations.remove(confirmation_key);
+
+            let mut transaction = self.transactions.get(transaction_id)
+                .ok_or(Error::InvalidTransactionId)?;
+        
+            if transaction.executed {
+                return Err(Error::TransactionAlreadyExecuted)
+            }
+
+            transaction.num_confirmations -= 1;
+            self.transactions.insert(transaction_id, &transaction);
+
+            Self::env().emit_event(RevokeConfirmation {
+                owner: caller,
+                transaction_id: transaction_id,
+            });
+
+            Ok(())
         }
 
-        /// We test a simple use case of our contract.
-        #[ink::test]
-        fn it_works() {
-            let mut multi_sig_wallet = MultiSigWallet::new(false);
-            assert_eq!(multi_sig_wallet.get(), false);
-            multi_sig_wallet.flip();
-            assert_eq!(multi_sig_wallet.get(), true);
+        #[ink(message)]
+        pub fn execute_transaction(&mut self, transaction_id: TransactionId) -> Result<()> {
+            let caller = Self::env().caller();
+            self.ensure_owner(caller)?;
+
+            let mut transaction = self.transactions.get(transaction_id)
+                .ok_or(Error::InvalidTransactionId)?;
+
+            if transaction.executed {
+                return Err(Error::TransactionAlreadyExecuted)
+            }
+
+            if transaction.num_confirmations < self.threshold {
+                return Err(Error::InsufficientConfirmations) 
+            }
+
+            transaction.executed = true;
+            self.transactions.insert(transaction_id, &transaction);
+
+            if self.env().transfer(transaction.to, transaction.value).is_err() {
+                return Err(Error::TransactionFailed)
+            }
+
+            Ok(())
+        }
+
+        #[ink(message)]
+        pub fn get_transaction(&self, transaction_id: TransactionId) -> Result<Transaction> {
+            let transaction = self.transactions.get(transaction_id)
+                .ok_or(Error::InvalidTransactionId)?;
+                
+            Ok(transaction)
+        }
+
+        fn ensure_owner(&self, owner: AccountId) -> Result<()> {
+            self.owners.get(owner).ok_or(Error::InvalidOwner)
         }
     }
 }
